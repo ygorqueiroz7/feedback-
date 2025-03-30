d2a598359e0820399a6fdbc2ba342a4c76e5f200
ygorqueiroz251 <ygorqueiroz251@gmail.com>
2025-03-30 14:33:13 +0000

    fim da criação e estilização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,22 @@
 import iconStar from "./images/icon-star.svg"
+import illustrationThankYou from "./images/illustration-thank-you.svg"
 import { useState } from "react"
 
 export function App() {
-  const [submited, setSubmit] = useState(false)
-  const [rateNote, setRateNote] = useState (0)
- 
-  
+  const [notaAvalicao, setNotaAvaliacao] = useState (0)
+  const [submited, setSubmited] = useState(false)
+
+  function handleMudarNotaAvalicao(nota) {
+    setNotaAvaliacao(nota)
+  }
+
   function handleSubmit() {
-    if (rateNote !==0) {
-      setSubmit(true)
+    if(notaAvalicao !==0) {
+      setSubmited(true)
       return
     }
 
-    alert("Plase, choose a rate button!")
-  }
-
-  function handleChangeRateNote(value) {
-    setRateNote(value)
-  }
-
-  function handleAlterarNotaAvaliacao(nota) {
-    setNotaAvaliacao(nota)
-
-    console.log(nota)
+    alert("Please, choose a note!")
   }
 
   return (
@@ -38,17 +32,26 @@ export function App() {
 
 
         <div className="flex justify-between mb-6">
-          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:text-white" onClick={() => handleChangeRateNote (1)}/>
-          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:text-white" onClick={() => handleChangeRateNote (2)}/>
-          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:text-white" onClick={() => handleChangeRateNote (3)}/>
-          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:text-white" onClick={() => handleChangeRateNote (4)}/>
-          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-grey focus:text-white" onClick={() => handleChangeRateNote (5)}/>
+          <input type="button" value={1} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvalicao (1)}/>
+          <input type="button" value={2} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvalicao (2)}/>
+          <input type="button" value={3} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvalicao (3)}/>
+          <input type="button" value={4} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvalicao (4)}/>
+          <input type="button" value={5} className="bg-dark-blue w-10.5 h-10.5 rounded-full text-medium-grey text-sm font-bold focus:bg-medium-grey focus:text-white" onClick={() => handleMudarNotaAvalicao (5)}/>
         </div>
 
-        <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl font-bold tracking-1" onClick={handleSubmit}>Submit</button>
+        <button  onClick={handleSubmit} className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
       </div>
     ) : (
-      <p>asdasd</p>
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+      
+      <img className="mx-auto mb-6" src={illustrationThankYou} alt="illustration thank you" />
+
+      <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You select {notaAvalicao} out of 5</p>
+
+      <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
+      <p className="text-sm text-light-grey leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!</p>
+    
+      </div>
     )
   )
 }
